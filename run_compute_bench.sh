449a574b3b81f3b417178644c2f87b673db63305
Ishita <ishitapethkar@gmail.com>
2025-11-21 12:21:28 -0500

    run_compute_bench.sh

---
+++ run_compute_bench.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+#SBATCH --job-name=compute-bench-test       
+#SBATCH --output=compute-bench-test.out 
+#SBATCH --error=compute-bench-test.err  
+#SBATCH --mem=5g
+#SBATCH -n 1
+#SBATCH -t 01-00:00:00
+#SBATCH -p l40-gpu
+#SBATCH --qos=gpu_access
+#SBATCH --gres=gpu:1
+
+# Load Apptainer module
+module load apptainer
+
+# Path to Apptainer image
+IMAGE="/nas/longleaf/home/ishpet/stor674hw5/compute-bench.sif"
+
+# Run the container
+apptainer exec $IMAGE python compute_bench.py
